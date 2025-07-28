5f1347b2e5f507fb1e56429893f6e31f4cf4b128
LisaNan <lisa.nan.fsw@gmail.com>
2025-07-28 16:52:33 -0300

    Improved User Experience

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ function createWorkout(event) {
   event.preventDefault();
 
   let muscleGroup = document.querySelector("#muscle-group");
-  console.log(muscleGroup.value);
   let apiKey = "beac6ctb368bo560b6dfbc1164f16438";
   let prompt = `Create a home workout plan of two rounds of 15 minutes with a maximum of 7 exercises per round, no equipment needed and focusing only on strength exercises and only targeting the ${muscleGroup.value}muscles.`;
   let context = `You are a skilled AI assistant that has been trained as a physiotherapist and personal trainer. You are specialised in creating workout plans targeting only the requested musclegroup. Be short and to the point, whilst giving enough explanation for people to be able to follow along without extra research. Display your answer first giving a short summary of things to take into account and be mindfull of when executing these exercises, then listing the exercises with explanation and amount of time spent per exercise. Provide your answer following basic HTML structure without headers. Wrap the exercise names in a strong element. Make sure to have two br elements after the summary and one br element after every exercise.
@@ -26,7 +25,7 @@ function createWorkout(event) {
   axios.get(apiUrl).then(displayWorkoutPlan);
 
   let workoutPlan = document.querySelector("#workout-plan");
-  workoutPlan.innerHTML = "Your workout plan is being created... 🤩";
+  workoutPlan.innerHTML = `<div class="generating">Your ${muscleGroup.value}-focussed workout plan is being created... 🤩</div>`;
 }
 
 let workoutFormElement = document.querySelector("#workout-creater-form");
